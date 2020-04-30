d4179d7ebe5fb7e68c94a834355aabcc14c413a9
Gametrain88 <33637988+Gametrain88@users.noreply.github.com>
2020-04-29 22:11:34 -0500

    A Rigidbody JumpPad
    This JumpPad works only for RigidBodies. It works by accessing the rigidbody component of the object coming in contact with it and adding a force to it in the +y-axis. This makes the colliding object bounce upward; acting against gravity.

---
+++ JumpPadScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpPadScript : MonoBehaviour
+{
+    //This is code to show a slider in the inspector view so that you can
+    //change the value of the bounce height without coming back into the IDE.
+    [Range(100, 1000)]
+    public float bounceHeight; 
+
+    void OnCollisionEnter(Collision collision)
+    {
+        GameObject bouncer = collision.gameObject;
+        Rigidbody rb = bouncer.GetComponent<Rigidbody>();
+        rb.AddForce(Vector3.up * bounceHeight);
+    }
+
+    /*This code works on a gameobject that contains a rigidbody. And
+     it also suggests that the upward direction of you game is the (positive0 y-axis
+     If there are any problems just let me know ;)  */
+}
