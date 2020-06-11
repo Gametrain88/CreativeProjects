65444af6c1182f7fc1968df454fe2550d87e8389
Gametrain88 <33637988+Gametrain88@users.noreply.github.com>
2020-06-10 20:33:17 -0500

    Add files via upload

---
+++ OneWayTeleporterScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OneWayTeleporterScript : MonoBehaviour
+{
+    public GameObject[] teleporterLocations;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Vector3 newPos = new Vector3(teleporterLocations[1].transform.position.x, 
+            teleporterLocations[1].transform.position.y + 0.8f, teleporterLocations[1].transform.position.z);
+        other.gameObject.transform.position = newPos;
+    }
+
+
+}
